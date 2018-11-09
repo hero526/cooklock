95767c2f32f82b01925a718599c2d57d41f1ee42
SeMin <ohsemin4@gmail.com>
2018-11-09 19:39:20 +0900

    Ingredient actvity basic

--- app/src/main/java/com/example/semin/cooklock/IngredientActivity.java
+++ app/src/main/java/com/example/semin/cooklock/IngredientActivity.java
@@ -1,13 +1,28 @@
 package com.example.semin.cooklock;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class IngredientActivity extends AppCompatActivity {
-
+    EditText ingredient_text;
+    Button select_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredient);
+        ingredient_text = (EditText)findViewById(R.id.ingredient);
+        select_button = (Button)findViewById(R.id.SelectButton);
+        select_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(IngredientActivity.this,RecipeActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
