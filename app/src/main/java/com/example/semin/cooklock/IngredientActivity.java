2b3400a2b934476d03a194936bc71b739951b688
SeMin <ohsemin4@gmail.com>
2018-11-09 19:19:42 +0900

    create basic Actvity

---
+++ app/src/main/java/com/example/semin/cooklock/IngredientActivity.java
@@ -0,0 +1,13 @@
+package com.example.semin.cooklock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IngredientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingredient);
+    }
+}
