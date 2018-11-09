cd36fd4d85c7a94ad7a044e0c5392454912e2e41
SeMin <ohsemin4@gmail.com>
2018-11-09 20:11:27 +0900

    ingredientActivity Add

--- app/src/main/java/com/example/semin/cooklock/IngredientActivity.java
+++ app/src/main/java/com/example/semin/cooklock/IngredientActivity.java
@@ -4,17 +4,25 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class IngredientActivity extends AppCompatActivity {
-    EditText ingredient_text;
+    AutoCompleteTextView ingredient_text;
     Button select_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredient);
-        ingredient_text = (EditText)findViewById(R.id.ingredient);
+        ingredient_text = (AutoCompleteTextView) findViewById(R.id.ingredient);
+        String[] ingredient_list = {"파","양파","마늘","김치","오징어","아몰라나도"};
+        ingredient_text.setAdapter(
+                new ArrayAdapter<String>(
+                        IngredientActivity.this,R.layout.support_simple_spinner_dropdown_item,ingredient_list
+                )
+        );
         select_button = (Button)findViewById(R.id.SelectButton);
         select_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +31,5 @@ public class IngredientActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-
     }
 }
