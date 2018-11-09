c4ee196f9cec8ee1819676b47a8731fa2f6c6bda
Seo <ckdgh526@naver.com>
2018-11-09 19:31:38 +0900

    Main basic layout

--- app/src/main/java/com/example/semin/cooklock/MainActivity.java
+++ app/src/main/java/com/example/semin/cooklock/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.semin.cooklock;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showIngredient(View view) {
+        Intent i = new Intent(this, IngredientActivity.class);
+        startActivity(i);
+    }
+
+    public void showRecipe(View view) {
+        Intent i = new Intent(this, RecipeActivity.class);
+        startActivity(i);
     }
 }
