95f75226d12d5bdaa230e2e07559d913fdb7374a
SeMin <ohsemin4@gmail.com>
2018-11-16 03:38:50 +0900

    adapter imageview add

--- app/src/main/java/com/example/semin/cooklock/MyListAdapter.java
+++ app/src/main/java/com/example/semin/cooklock/MyListAdapter.java
@@ -1,6 +1,8 @@
 package com.example.semin.cooklock;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,11 +13,16 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MyListAdapter extends BaseAdapter {
     private ArrayList<Recipe_Basic> listViewItemList = new ArrayList<Recipe_Basic>() ;
-
+    Bitmap bitmap;
     // ListViewAdapter의 생성자
     public MyListAdapter() {
 
@@ -40,17 +47,47 @@ public class MyListAdapter extends BaseAdapter {
         }
 
         // 화면에 표시될 View(Layout이 inflate된)으로부터 위젯에 대한 참조 획득
-       // ImageView iconImageView = (ImageView) convertView.findViewById(R.id.recipe_image) ;
-        TextView iconImageView = (TextView) convertView.findViewById(R.id.recipe_image) ;
+
+
         TextView titleTextView = (TextView) convertView.findViewById(R.id.recipe_name) ;
 
         // Data Set(listViewItemList)에서 position에 위치한 데이터 참조 획득
-        Recipe_Basic recipe_basic = listViewItemList.get(position);
+        final Recipe_Basic recipe_basic = listViewItemList.get(position);
+        ImageView imageView = (ImageView)convertView.findViewById(R.id.recipe_image);
+
+
+        Thread mThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(recipe_basic.getRecipe_Image());
+
+                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                    conn.setDoInput(true);
+                    conn.connect();
+
+                    InputStream is = conn.getInputStream();
+                    bitmap = BitmapFactory.decodeStream(is);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        mThread.start();
+        try {
+            mThread.join();
+            imageView.setImageBitmap(bitmap);
+        } catch (InterruptedException e) {
+        }
+
 
         // 아이템 내 각 위젯에 데이터 반영
-        titleTextView.setText(recipe_basic.getRecipe_Image());
-  //     iconImageView.setImageDrawable(recipe_basic.getRecipe_Image());
-        titleTextView.setText(recipe_basic.getRecipe_name());
+      //  titleTextView.setText(recipe_basic.getRecipe_Image());
+       //iconImageView.setImageBitmap();
+       titleTextView.setText(recipe_basic.getRecipe_name());
 
         return convertView;
     }
@@ -81,12 +118,14 @@ public class MyListAdapter extends BaseAdapter {
     public void addItem(String recipe_id, String recipe_name, String recipe_expain, String recipe_type, String recipe_time, String recipe_kcal, String recipe_level, String recipe_Image)
     {
         Recipe_Basic item = new Recipe_Basic(recipe_id,recipe_name,recipe_expain,recipe_type,recipe_time,recipe_kcal,recipe_level,recipe_Image);
+
         item.setRecipe_id(recipe_id);
         item.setRecipe_name(recipe_name);
         item.setRecipe_expain(recipe_expain);
         item.setRecipe_type(recipe_type);
         item.setRecipe_kcal(recipe_kcal);
         item.setRecipe_Image(recipe_Image);
+
         listViewItemList.add(item);
     }
 
