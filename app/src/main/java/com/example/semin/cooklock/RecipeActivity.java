8188e315ab7dbc5e025485495f21e3e1b80c51e2
HongKi <HongSeongGi@github.com>
2018-11-23 22:25:03 +0900

    add recipe search  function

--- app/src/main/java/com/example/semin/cooklock/RecipeActivity.java
+++ app/src/main/java/com/example/semin/cooklock/RecipeActivity.java
@@ -11,6 +11,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -24,6 +26,8 @@ public class RecipeActivity extends AppCompatActivity {
 
     ListView listView;
     MyListAdapter Myadapter;
+    EditText editText ;
+    ImageButton button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +62,35 @@ public class RecipeActivity extends AppCompatActivity {
                 Intent i = new Intent(getApplicationContext(),CookingActivity.class);
                 i.putExtra("foodid",recipe.getRecipe_id());
                 startActivity(i);
+            }
+        });
+
+        editText=(EditText)findViewById(R.id.editText);
+
+        button = (ImageButton)findViewById(R.id.imageButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editText.getText().toString();
+
+                InputStream inputStream = getResources().openRawResource(R.raw.recipe_basic);
+                Myadapter.listViewItemList.clear();
+                Scanner scanner = new Scanner(inputStream);
+                scanner.nextLine();
+                while (scanner.hasNextLine()) {
+                    String[] data = scanner.nextLine().split(",");
+                    try {
+
+                        if (data[1].contains(name))
+                            Myadapter.addItem(data[0], data[1], data[2], data[4], data[7], data[8], data[10], data[13]);
+                    } catch (NumberFormatException e) {
+                        continue;
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        continue;
+                    }
+                }
+                scanner.close();
+                Myadapter.notifyDataSetChanged();
             }
         });
     }
